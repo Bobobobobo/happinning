baeda8733d294ad3d43f40eb1d772e9e73ff9f16
Llvve <Llvve@192.168.1.38>
2014-06-11 19:37:23 +0700

    Test commit GIT

--- routes/getPins.js
+++ routes/getPins.js
@@ -3,24 +3,24 @@
  */
 
 function getPins(res, latitude, longitude, maxDistance, mongodb) {
-//	mongodb.collection('pins').ensureIndex({coordinates:'2d'});
-//	mongodb.collection('pins').find(
-//			{ coordinates :
-//				{ $near : [longitude, latitude]}
-//			}).toArray(function(err, records) {
-//				if (err) {
-//					throw err;
-//				}
-////				console.log("Record get as "+records[0]._id);
-//				res.send(records);
-//			});
+	mongodb.collection('pins').ensureIndex({coordinates:'2d'});
+	mongodb.collection('pins').find(
+			{ coordinates :
+				{ $near : [longitude, latitude]}
+			}).toArray(function(err, records) {
+				if (err) {
+					throw err;
+				}
+//				console.log("Record get as "+records[0]._id);
+				res.send(records);
+			});
 	
-	mongodb.collection('pins').find().toArray(function(err, records) {
-		if (err) {
-			throw err;
-		}
-		res.send(records);
-	});
+//	mongodb.collection('pins').find().toArray(function(err, records) {
+//		if (err) {
+//			throw err;
+//		}
+//		res.send(records);
+//	});
 }
 
 module.exports = {
