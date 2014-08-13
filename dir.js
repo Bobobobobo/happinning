e3d5d0884489d8d79169b31cc741556c573fc330
Llvve <Llvve@192.168.20.188>
2014-08-13 17:08:26 +0700

    fixed dir from localhost to aws server

--- dir.js
+++ dir.js
@@ -1,6 +1,6 @@
-var __dir = '/Users/Llvve/mongodb/content/';
-var __urlPrefixImage = 'http://192.168.1.105:3000/image/';
-var __urlPrefixVideo = 'http://192.168.1.105:3000/video/';
+var __dir = '/home/ubuntu/content/';
+var __urlPrefixImage = 'http://54.179.16.196:3000/image/';
+var __urlPrefixVideo = 'http://54.179.16.196:3000/video/';
 
 exports.dir = __dir;
 exports.urlPrefixImage = __urlPrefixImage;
