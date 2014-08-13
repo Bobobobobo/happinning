4366710b3e1e65a46f8d48cd9d2e2c087c52cae4
Llvve <Llvve@192.168.20.188>
2014-08-13 20:48:17 +0700

    test ensure 2d index

--- app.js
+++ app.js
@@ -139,6 +139,7 @@ MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
 		if(err) {
 			throw err;
 		}
+		console.log("Ensure Index 2d ");
 	});
 	console.log("Connected to Database ");
 });
