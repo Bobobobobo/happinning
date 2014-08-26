b50e97b7037a4e784f52c7f1dbb1b6f08c7cbfb3
nonamexp9 <tanthawatk@Tanthawats-MacBook-Pro.local>
2014-08-26 23:32:31 +0700

    fix bridging header

---
+++ Happining_V2.1/Happening_V2.1-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Happening_V2.1-Bridging-Header.h
+//  Happining_V2.1
+//
+//  Created by Tanthawat Khemavast on 26/8/14.
+//  Copyright (c) 2014 Kan Boonprakub. All rights reserved.
+//
+
+#import "SWRevealViewController.h"
--- Happining_V2.1/Happening_V2.1/<Happining_V2.1>-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  <Happining_V2.1>-Bridging-Header.h
-//  Happining_V2.1
-//
-//  Created by Kan Boonprakub on 8/16/2557 BE.
-//  Copyright (c) 2557 Kan Boonprakub. All rights reserved.
-//
-
-#import "SWRevealViewController.h"
